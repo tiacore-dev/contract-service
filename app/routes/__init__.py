4b4e078446561fab606de8a8a9a81815db4402db
Viktoria <v.s.polikanova@gmail.com>
2025-06-16 18:20:26 +0700

    первый деплой

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from tiacore_lib.routes.auth_route import auth_router
 from tiacore_lib.routes.company_route import company_router
+from tiacore_lib.routes.invite_route import invite_router
+from tiacore_lib.routes.register_route import register_router
+from tiacore_lib.routes.user_route import user_router
 
 from .contract_route import contract_router
 from .contract_type_route import contract_type_router
@@ -8,7 +11,10 @@ from .monitoring_route import monitoring_router
 
 
 def register_routes(app: FastAPI):
-    app.include_router(auth_router, prefix="/api/auth", tags=["Auth"])
+    app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+    app.include_router(invite_router, prefix="/api", tags=["Invite"])
+    app.include_router(register_router, prefix="/api", tags=["Register"])
+    app.include_router(user_router, prefix="/api/users", tags=["Users"])
     app.include_router(company_router, prefix="/api/companies", tags=["Companies"])
     app.include_router(monitoring_router, tags=["Monitoring"])
     app.include_router(
