acd080fe26065412061025b1e0a090280b3d44dc
Viktoria <v.s.polikanova@gmail.com>
2025-07-09 21:35:55 +0700

    создание типа контракта

--- app/utils/db_helpers.py
+++ app/utils/db_helpers.py
@@ -9,18 +9,14 @@ async def drop_all_tables():
     """)
     async with in_transaction() as tx:
         for table in tables:
-            await tx.execute_query(
-                f'DROP TABLE IF EXISTS "{table["tablename"]}" CASCADE;'
-            )
+            await tx.execute_query(f'DROP TABLE IF EXISTS "{table["tablename"]}" CASCADE;')
 
 
 async def create_data():
     from app.database.models import ContractType
 
     try:
-        await ContractType(
-            id="delivery", name="Договор оказания курьерских услуг", colour="#b70094"
-        )
+        await ContractType.get_or_create(id="delivery", name="Договор оказания курьерских услуг", colour="#b70094")
 
     except Exception as e:
         print(f"Exception: {e}")
